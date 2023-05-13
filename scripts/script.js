95039b134db9f3860c307d6ebddb5dd56d6f59e0
Leonid Temniy <leonidtemniy@yandex.ru>
2023-05-13 23:15:52 +0300

    fixed bug's

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,25 @@
-const editButton = document.querySelector(".profile__edit-button"); //объявляем переменную для кнопки редактирования профиля
-const editPopup = document.querySelector(".editpopup"); //объявляем переменную для всего попапа редактирования профиля
-const editPopupCloseButton = editPopup.querySelector(".popup__closebutton"); //объявляем переменную для
-const nameInput = editPopup.querySelector(".popup__name"); //объявляем переменную для ипута имени профиля
-const professionInput = editPopup.querySelector(".popup__profession"); //объявляем переменную для инпута профессии
-const editPopupForm = editPopup.querySelector(".popup__form"); //объявляем переменную для формы редактирования профиля
-const profileTitle = document.querySelector(".profile__title"); //объявляем переменную для значения имени
-const profileProfession = document.querySelector(".profile__subtitle"); //объявляем переменную для значения профессии
+const editButton = document.querySelector('.profile__edit-button'); //объявляем переменную для кнопки редактирования профиля
+const editPopup = document.querySelector('.editpopup'); //объявляем переменную для всего попапа редактирования профиля
+const editPopupCloseButton = editPopup.querySelector('.popup__closebutton'); //объявляем переменную для
+const nameInput = editPopup.querySelector('.popup__input_type_name'); //объявляем переменную для ипута имени профиля
+const professionInput = editPopup.querySelector('.popup__input_type_profession'); //объявляем переменную для инпута профессии
+const editPopupForm = editPopup.querySelector('.popup__form'); //объявляем переменную для формы редактирования профиля
+const profileTitle = document.querySelector('.profile__title'); //объявляем переменную для значения имени
+const profileProfession = document.querySelector('.profile__subtitle'); //объявляем переменную для значения профессии
 
 function closePopup() {
-  editPopup.classList.remove("popup_opened"); // создаем функцию на закрытие попапа
+  editPopup.classList.remove('popup_opened'); // создаем функцию на закрытие попапа
 }
 
-editButton.addEventListener("click", function () {
+editButton.addEventListener('click', function () {
   // добавляем ивентлисинер по клику на кнопку редактирования профиля для открытия попапа
-  editPopup.classList.add("popup_opened");
+  editPopup.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent; // пишем текстконтент тайтла в инпут
   professionInput.value = profileProfession.textContent; // пишем текстконтент профессии в инпут
 });
-editPopupCloseButton.addEventListener("click", closePopup); //закрытие попапа по клику
+editPopupCloseButton.addEventListener('click', closePopup); //закрытие попапа по клику
 
-editPopupForm.addEventListener("submit", function (evt) {
+editPopupForm.addEventListener('submit', function (evt) {
   // добавляем сабмит на форму
   evt.preventDefault(); //прерываем обновление страницы и отправку на сервер
   profileTitle.textContent = nameInput.value; // присваеваем значениее из инпута
