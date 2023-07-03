4ef845d4169b7d3213c51215d68ad652377d1495
Leonid Temniy <leonidtemniy@yandex.ru>
2023-07-04 00:24:46 +0300

    подготовка к 7 ПР

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,81 @@
-const editButton = document.querySelector('.profile__edit-button'); //объявляем переменную для кнопки редактирования профиля
-const editPopup = document.querySelector('.editpopup'); //объявляем переменную для всего попапа редактирования профиля
-const editPopupCloseButton = editPopup.querySelector('.popup__closebutton'); //объявляем переменную для кнопки закрытия профиля
-const nameInput = editPopup.querySelector('.popup__input_type_name'); //объявляем переменную для ипута имени профиля
-const professionInput = editPopup.querySelector('.popup__input_type_profession'); //объявляем переменную для инпута профессии
+////Переменные и константы
 
-const editPopupForm = editPopup.querySelector('.popup__form'); //объявляем переменную для формы редактирования профиля
-
-const profileTitle = document.querySelector('.profile__title'); //объявляем переменную для значения имени
-const profileProfession = document.querySelector('.profile__subtitle'); //объявляем переменную для значения профессии
-
-const addPopup = document.querySelector('.addpopup'); //объявляем переменную для всего попапа добавленияя мест
-const addButton = document.querySelector('.profile__add-button'); // объявляем переменную для открытия попапа добавления мест
-const addPopupCloseButton = addPopup.querySelector('#addPopupCloseButton'); // объявляем переменную для кнопки закрытия мест
-
-const addPopupForm = addPopup.querySelector('#popupAddCardForm'); //объявляем переменную для формы добавления мест
-const popups = document.querySelectorAll('.popup'); // выбираем все попапы и объявляем переменную
+//объявляем переменную для кнопки редактирования профиля
+const editButton = document.querySelector('.profile__edit-button');
+//объявляем переменную для всего попапа редактирования профиля
+const editPopup = document.querySelector('.editpopup');
+//объявляем переменную для кнопки закрытия профиля
+const editPopupCloseButton = editPopup.querySelector('.popup__closebutton');
+//объявляем переменную для ипута имени профиля
+const nameInput = editPopup.querySelector('.popup__input_type_name');
+//объявляем переменную для инпута профессии
+const professionInput = editPopup.querySelector('.popup__input_type_profession');
+//объявляем переменную для формы редактирования профиля
+const editPopupForm = editPopup.querySelector('.popup__form');
+//объявляем переменную для значения имени
+const profileTitle = document.querySelector('.profile__title');
+//объявляем переменную для значения профессии
+const profileProfession = document.querySelector('.profile__subtitle');
+//объявляем переменную для всего попапа добавленияя мест
+const addPopup = document.querySelector('.addpopup');
+//объявляем переменную для открытия попапа добавления мест
+const addButton = document.querySelector('.profile__add-button');
+//объявляем переменную для кнопки закрытия мест
+const addPopupCloseButton = addPopup.querySelector('#addPopupCloseButton');
+//объявляем переменную для формы добавления мест
+const addPopupForm = addPopup.querySelector('#popupAddCardForm');
+//объявляем переменную всех Попапов
+const popups = document.querySelectorAll('.popup');
 
 ////функции закрытия и открытия
-///функция закрытия по Esc
+
+//функция закрытия по Esc
 function closeByEsc(evt) {
   if (evt.key === 'Escape') {
     const openedPopup = document.querySelector('.popup_opened');
     closePopup(openedPopup);
   }
 }
-/// создаем функцию на закрытие попапа
+//функцию на закрытие попапа
 function closePopup(anyPopup) {
   anyPopup.classList.remove('popup_opened');
   document.addEventListener('keydown', closeByEsc);
 }
-/// создаем функцию на открытие попапа
+//функцию на открытие попапа
 function openPopup(anyPopup) {
   anyPopup.classList.add('popup_opened');
   document.addEventListener('keydown', closeByEsc);
 }
-
+//метод прохода по всем попапам для закрытия кликом по оверлею
 popups.forEach(popup => {
-  // закрытие по клику
   popup.addEventListener('click', click => {
-    //закрываем по клику
     if (click.target === popup) {
       closePopup(popup);
     }
   });
 });
-////
-///фунция очищения
-addButton.addEventListener('click', () => openPopup(addPopup)); // добавляем ивентлисинер на кнопку addButton на открытие попапа добавления мест
-
+// добавляем ивентлисинер на кнопку addButton на открытие попапа добавления мест
+addButton.addEventListener('click', () => openPopup(addPopup));
+// добавляем ивентлисинер по клику на кнопку editButton для открытия попапа редактирования
 editButton.addEventListener('click', function () {
-  openPopup(editPopup); // добавляем ивентлисинер по клику на кнопку редактирования профиля для открытия попапа
-  nameInput.value = profileTitle.textContent; // пишем текстконтент тайтла в инпут
-  professionInput.value = profileProfession.textContent; // пишем текстконтент профессии в инпут
+  openPopup(editPopup);
+  nameInput.value = profileTitle.textContent;
+  professionInput.value = profileProfession.textContent;
 });
-editPopupCloseButton.addEventListener('click', () => closePopup(editPopup)); //закрытие попапа профиля
-addPopupCloseButton.addEventListener('click', () => closePopup(addPopup)); // закрытие попапа мест
+//закрытие попапов профиля и добавления мест
+editPopupCloseButton.addEventListener('click', () => closePopup(editPopup));
+addPopupCloseButton.addEventListener('click', () => closePopup(addPopup));
 
 const cardTemplate = document.querySelector('#element-template').content.querySelector('.element');
 const elements = document.querySelector('.elements'); // объявляем переменную элементс
-const popupImg = document.querySelector('.popup-img'); //
+const popupImg = document.querySelector('.popup-img');
 
 const popupImgTitle = popupImg.querySelector('.popup-img__title');
 const popupImgPhoto = popupImg.querySelector('.popup-img__photo');
 
-/** @param {{name:string; link:string}} createCard  */
+// функция создания карточки
 const createCard = ({ name, link }) => {
-  // функция создания карточки
   const newCard = cardTemplate.cloneNode(true);
-  /** @type {HTMLSpanElement} */
   const newCardName = newCard.querySelector('.element__title'); // объявляем переменную с названием места
   newCardName.textContent = name; // присваеваем текст названия из масива
   const newCardPhoto = newCard.querySelector('.element__photo'); // объявляем переменную с фото
@@ -97,19 +105,19 @@ cardData.forEach(card => {
   elements.append(newCard);
 });
 
+// добавляем сабмит на форму edit
 editPopupForm.addEventListener('submit', function (evt) {
-  // добавляем сабмит на форму
   evt.preventDefault(); //прерываем обновление страницы и отправку на сервер
   profileTitle.textContent = nameInput.value; // присваеваем значениее из инпута
   profileProfession.textContent = professionInput.value; // присваеваем значениее из инпута
   closePopup(editPopup); //закрытие попапа после сабмита формы
 });
 
+// добавляем сабмит на форму add
 addPopupForm.addEventListener('submit', evt => {
   evt.preventDefault(); // прерываем обновление страницы и отправку на сервер
   const form = evt.target;
   const formData = new FormData(form);
-  /**@type {HTMLFormElement} */
   const values = Object.fromEntries(formData);
   const placeValue = values['place'];
   const linkValue = values['img-path'];
