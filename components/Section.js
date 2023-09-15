1c7ef74a0ab79c3e3e55d3e341f864faf931626d
Leonid Temniy <leonidtemniy@yandex.ru>
2023-09-15 20:34:13 +0300

    PW8 классы работают

--- components/Section.js
+++ components/Section.js
@@ -11,7 +11,7 @@ export class Section {
   constructor({ items, renderer }, containerSelector) {
     this._items = items;
     this._renderer = renderer;
-    this._container = document.querySelector(containerSelector);
+    this._container = containerSelector;
   }
   renderItems() {
     this._items.forEach(item => {
--- pages/newindex.js
+++ pages/newindex.js
@@ -38,10 +38,51 @@ popupWithProfilInfo.setEventListeners();
 editButton.addEventListener('click', () => {
   popupWithProfilInfo.open();
   const userNameAndProfession = userInfo.getUserInfo();
-
   nameInput.value = userNameAndProfession.name;
   professionInput.value = userNameAndProfession.info;
 });
 
 const formSelectorsEdit = new FormValidator(settings, editPopupForm);
 formSelectorsEdit.enableValidation();
+
+//=====Добавление карточек с кнопки=====//
+
+const popupWithAddCard = new PopupWithForm('.addpopup', inputs => {
+  const cardInfo = {
+    name: inputs['place'],
+    link: inputs['img-path']
+  };
+  newSection.addItem(newCard(cardInfo));
+  popupWithAddCard.close();
+});
+popupWithAddCard.setEventListeners();
+
+addButton.addEventListener('click', () => {
+  popupWithAddCard.open();
+});
+
+const formSelectorsAdd = new FormValidator(settings, addPopupForm);
+formSelectorsAdd.enableValidation();
+
+//=====Добавление карточек из cardData =====//
+const handleCardClick = (name, link) => {
+  popupWithImage.open(name, link);
+};
+
+const newCard = data => {
+  const card = new Card(data, '#element-template', handleCardClick);
+  const cardElement = card.generateCard();
+  return cardElement;
+};
+
+const popupWithImage = new PopupWithImage('.popup-img');
+popupWithImage.setEventListeners();
+
+const newSection = new Section(
+  {
+    items: cardData,
+    renderer: items => newSection.addItem(newCard(items))
+  },
+  cardSection
+);
+newSection.renderItems();
